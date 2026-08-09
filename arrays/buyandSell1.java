eb645d092044028a9b66c75fbb210f6ba8e0ff57
Siddharth_Kumar_Kothiyal <siddharthkothiyal30@gmail.com>
2026-08-09 20:10:09 +0530

    new question

---
+++ arrays/buyandSell1.java
@@ -0,0 +1,18 @@
+  
+  package arrays;
+public class buyandSell1 {
+ public int maxProfit(int[] prices) {
+        int mini=  prices[0];
+       int  profit = 0;
+
+
+        for(int i= 1;i < prices.length ; i++){
+            profit= Math.max(profit , prices[i]-mini);
+            mini = Math.min(mini , prices[i]);
+        }
+
+        return profit;
+        
+    }
+    
+}
